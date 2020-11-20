eb60a44c8bbe3b177746168e887adcbfeef7d261
Lacey Walker <bluelarksong@gmail.com>
2020-11-20 13:32:41 -0600

    see your teemates

--- Program.cs
+++ Program.cs
@@ -13,17 +13,17 @@ namespace Heist
             string name = null;
             //creating an empty list
 
-            List<TeamMember> Teammates = new List<TeamMember>();
+            List<TeamMember> teammates = new List<TeamMember>();
             // while loop runs through the team members
             while (name != "")
             {
-                Console.WriteLine("Add your new team members name: ");
+                Console.WriteLine("Add your new team members name , or press enter to close and see your tea: ");
                 //breaks code if user does not enter a name
+                name = Console.ReadLine();
                 if (name == "")
                 {
                     break;
                 }
-                name = Console.ReadLine();
                 Console.WriteLine("Enter skill level: ");
                 int skillLevel = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter courage factor, pick between 0.0 - 2.0 : ");
@@ -36,12 +36,12 @@ namespace Heist
                     _courageFactor = courageFactor
                 };
 
-                Teammates.Add(newTeammate);
+                teammates.Add(newTeammate);
 
-                foreach (TeamMember Teammate in Teammates)
+                foreach (TeamMember teammate in teammates)
                 {
-                    Console.WriteLine($"Team member info: {name} has a skill level of {skillLevel} and a courage factor of {courageFactor}");
-                }
+                    Console.WriteLine($"Team member info: {teammate._name} has a skill level of {teammate._skillLevel} and a courage factor of {teammate._courageFactor}");
+                };
 
             };
 
