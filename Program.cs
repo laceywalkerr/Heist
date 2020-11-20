7d129146785573ece98d67f776ede77dfad5ae88
Lacey Walker <bluelarksong@gmail.com>
2020-11-20 11:47:19 -0600

    working on phase2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Heist
 {
@@ -6,16 +8,48 @@ namespace Heist
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Plan Your Heist!");
-            Console.WriteLine("Add your new team members name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter skill level: ");
-            int skillLevel = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter courage factor, pick between 0.0 - 2.0 : ");
-            decimal courageFactor = int.Parse(Console.ReadLine());
+            string name = null;
+            //creating an empty list
 
-            Console.WriteLine($"Team member info: {name} has a skill level of {skillLevel} and a courage factor of {courageFactor}");
+            List<TeamMember> Teammates = new List<TeamMember>();
+            // while loop runs through the team members
+            while (name != "")
+            {
+                Console.WriteLine("Add your new team members name: ");
+                //breaks code if user does not enter a name
+                if (name == "")
+                {
+                    break;
+                }
+                name = Console.ReadLine();
+                Console.WriteLine("Enter skill level: ");
+                int skillLevel = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter courage factor, pick between 0.0 - 2.0 : ");
+                decimal courageFactor = int.Parse(Console.ReadLine());
+                //creating the team members
+                TeamMember newTeammate = new TeamMember()
+                {
+                    _name = name,
+                    _skillLevel = skillLevel,
+                    _courageFactor = courageFactor
+                };
+
+                Teammates.Add(newTeammate);
+
+                foreach (TeamMember Teammate in Teammates)
+                {
+                    Console.WriteLine($"Team member info: {name} has a skill level of {skillLevel} and a courage factor of {courageFactor}");
+                }
+
+            };
+
+            // foreach (Teammate in Teammates)
+            // {
+            //     Console.WriteLine($"Team member info: {name} has a skill level of {skillLevel} and a courage factor of {courageFactor}");
+            // }
+
         }
-
     }
 }
