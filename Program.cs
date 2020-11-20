c9e802747ef1b1f08804b6bc2e33d7b05dace184
Lacey Walker <bluelarksong@gmail.com>
2020-11-20 10:48:25 -0600

    phase one attempted

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("Add your new team members name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter skill level: ");
+            string skillLevel = Console.ReadLine();
+            Console.WriteLine("Enter courage factor, pick between 0.0 - 2.0 : ");
+            string courageFactor = Console.ReadLine();
+
+            Console.WriteLine($"Team member info: {name} has a skill level of {skillLevel} and a courage factor of {courageFactor}");
         }
+
     }
 }
---
+++ Team.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Heist
+{
+    public class TeamMember
+    {
+        public string _name { get; set; }
+        public int _skillLevel { get; set; }
+        public decimal _courageFactor { get; set; }
+
+    }
+}
